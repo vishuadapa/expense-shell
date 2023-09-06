a967dac1bf8c0ee77e9300aa533a80ae31a033bb
vishuadapa <60673866+vishuadapa@users.noreply.github.com>
2023-09-06 16:07:09 -0500

    Repeat the same steps of demo on how to commit and push

---
+++ frontend.sh
@@ -0,0 +1,13 @@
+dnf install nginx -y
+systemctl enable nginx
+systemctl start nginx
+rm -rf /usr/share/nginx/html/*
+
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+
+cp expense.conf /etc/nginx/default.d/expense.conf
+
+systemctl restart nginx
