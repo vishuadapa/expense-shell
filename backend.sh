ea0a6db291375f94cfd10c4dd896d44b8d0c82b5
vishuadapa <60673866+vishuadapa@users.noreply.github.com>
2023-09-06 16:41:31 -0500

    Repeat the same steps of demo on how to commit and push

--- backend.sh
+++ backend.sh
@@ -1,9 +1,10 @@
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
 dnf install nodejs -y
+cp backend.service /etc/systemd/system/backend.service
+
 useradd expense
 mkdir /app
-
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
 cd /app
 unzip /tmp/backend.zip
@@ -11,12 +12,10 @@ unzip /tmp/backend.zip
 npm install
 
 systemctl daemon-reload
-
 systemctl enable backend
 systemctl start backend
 
 dnf install mysql -y
-
 mysql -h localhost -uroot -pExpenseApp@1 < /app/schema/backend.sql
 
 
